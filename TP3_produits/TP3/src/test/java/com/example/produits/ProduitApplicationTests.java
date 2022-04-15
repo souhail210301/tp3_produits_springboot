3d9879110232312c64d2f70a81b65d5cb09bf1aa
Souhail Abdennebi <100965022+souhail210301@users.noreply.github.com>
2022-04-15 01:13:28 +0100

    Add files via upload

---
+++ TP3_produits/TP3/src/test/java/com/example/produits/ProduitApplicationTests.java
@@ -0,0 +1,73 @@
+package com.example.produits;
+
+import java.util.Date;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+
+import com.example.produits.entities.Produit;
+import com.example.produits.repos.ProduitRepository;
+import com.example.produits.service.ProduitService;
+
+@SpringBootTest
+class ProduitApplicationTests {
+
+	@Autowired
+	private ProduitRepository produitRepository;
+	@Autowired
+	private ProduitService produitService ;
+	@Test
+	public void testCreateProduit() {
+	Produit prod = new Produit("PC Dell",2200.500,new Date());
+	produitRepository.save(prod);
+	}
+	
+	
+	@Test
+	public void testFindProduit()
+	{
+	Produit p = produitRepository.findById(1L).get();
+
+	System.out.println(p);
+	}
+	@Test
+	public void testUpdateProduit()
+	{
+	Produit p = produitRepository.findById(1L).get();
+	p.setPrixProduit(1000.0);
+	produitRepository.save(p);
+	}
+	@Test
+	public void testDeleteProduit()
+	{
+	produitRepository.deleteById(1L);;
+	}
+	@Test
+	public void testListerTousProduits()
+	{
+	List<Produit> prods = produitRepository.findAll();
+	for (Produit p : prods)
+	{
+	System.out.println(p);
+	}
+}
+	@Test
+	public void testFindByNomProduitContains()
+	{
+	Page<Produit> prods = produitService.getAllProduitsParPage(0,2);
+	System.out.println(prods.getSize());
+	System.out.println(prods.getTotalElements());
+
+
+	System.out.println(prods.getTotalPages());
+	prods.getContent().forEach(p -> {System.out.println(p.toString());
+	});
+	/*ou bien
+	for (Produit p : prods)
+	{
+	System.out.println(p);
+	} */
+	}}
